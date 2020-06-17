7d2e3070d0f809ee2f906101540ae7d9e907de93
test <test@tests-mbp>
2020-06-17 01:46:41 -0700

    committing Test2 to MyBranch

---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+
+public class Test2
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("First part of program");
+		String hungry = "yes";
+		if (hungry == "yes")
+			System.out.println("eat something....");
+		else
+		{
+			System.out.println("do work....");
+		}
+		System.out.println("end part of program");
+	}
+
+}
